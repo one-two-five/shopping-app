dc512b1de4bea1906fec08bc1cfa011b3fe29b7f
Mark Waters <mark.waters@johnlewis.co.uk>
2020-05-11 14:17:09 +0100

    add data files for turntables and speakers

---
+++ src/data/speakers.js
@@ -0,0 +1,86 @@
+const speakers = [
+    {
+        title: "JBL Control 1 (Black)",
+        info: "Speakers Per Pair",
+        stars: 5,
+        price: 59,
+        img: "https://www.richersounds.com/media/catalog/product/1/f/1f033999892a3645194c012d88742e10a90109ee_305621.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Wharfedale Diamond 9.0 (Black) ",
+        info: "Speakers Per Pair",
+        stars: 4.5,
+        price: 79,
+        img: "https://www.richersounds.com/media/catalog/product/e/b/eb70a5dacca8e469192b02189973c223a77ebb1b_306737.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Cambridge Audio Minx MIN12 (Black) ",
+        info: "Single Satellite Speaker",
+        stars: 4,
+        price: 89,
+        img: "https://www.richersounds.com/media/catalog/product/1/f/1fa36d89fc4d42113d581086842392c36cf89e1f_313731_v2.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Tibo Harmony 4 (Black)",
+        info: "Speakers Per Pair",
+        stars: 3.5,
+        price: 99,
+        img: "https://www.richersounds.com/media/catalog/product/a/9/a95180f37d901e27537ba7ddac84902927bd6b84_407069.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Mordaunt Short M10 (Black) ",
+        info: "Speakers Per Pair",
+        stars: 5,
+        price: 75,
+        img: "https://www.richersounds.com/media/catalog/product/9/1/912d6f3515ada697d1e718c8c8a5df92c4287dd7_312749_v2.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Arcam Muso (Silver)",
+        info: "Speakers Per Pair",
+        stars: 4.6,
+        price: 109,
+        img: "https://www.richersounds.com/media/catalog/product/f/0/f0c2348ff5f9a4b646dc9525fe24a924c430c265_405974_v4.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Mission LX1 (Walnut)",
+        info: "Speakers Per Pair",
+        stars: 4.3,
+        price: 79,
+        img: "https://www.richersounds.com/media/catalog/product/b/4/b4c237ce622dd4ffa3933ecb3d022355c4596c40_401758.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Mission LX1 (Black Wood) ",
+        info: "Speakers Per Pair",
+        stars: 5,
+        price: 89,
+        img: "https://www.richersounds.com/media/catalog/product/6/a/6afb1097f3f18ff946cf87841ec35bc2566a7620_401757.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Mordaunt Short M20 (Black) ",
+        info: "Speakers Per Pair",
+        stars: 5,
+        price: 139,
+        img: "https://www.richersounds.com/media/catalog/product/6/1/61b7e6c7553bd90782090e9b4fd21e6ce2d3f59c_312750.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Cambridge Audio SX50 (Walnut)",
+        info: "Speakers Per Pair",
+        stars: 4.2,
+        price: 139,
+        img: "https://www.richersounds.com/media/catalog/product/a/b/ab4172a72473166dc24b1547832edaac37a3d163_311503.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Q Acoustics Q3010i (White) ",
+        info: "Speakers Per Pair",
+        stars: 5,
+        price: 159,
+        img: "https://www.richersounds.com/media/catalog/product/5/5/55eca3601dd8f76397cd31c2a11ce95e91801654_402086_v2.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Q Acoustics Q3010i (Graphite)",
+        info: "Speakers Per Pair",
+        stars: 5,
+        price: 139,
+        img: "https://www.richersounds.com/media/catalog/product/6/8/68475f4de87be79333008732d071fe36139002a1_402084_v2.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    }
+]
---
+++ src/data/turntables.js
@@ -0,0 +1,86 @@
+const turntables = [
+    {
+        title: "Sony PSLX300 (Black)",
+        info: "USB Turntable",
+        stars: 5,
+        price: 59,
+        img: "https://www.richersounds.com/media/catalog/product/e/a/ea1326632866bdd724a1fe7819b50a669f4448f0_305172.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Marantz TT5005",
+        info: "Turntable",
+        stars: 4.5,
+        price: 79,
+        img: "https://www.richersounds.com/media/catalog/product/c/9/c96a7415e717ec44a943a65c824e0863c7bd9d65_310148.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Teac TN180BT (Black)",
+        info: "Bluetooth Turntable",
+        stars: 4,
+        price: 89,
+        img: "https://www.richersounds.com/media/catalog/product/9/3/934f76ef2f3e429b69c3671a5dbf16ca4d07d52e_402260.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Pro-Ject Primary E (Black)",
+        info: "Turntable",
+        stars: 3.5,
+        price: 99,
+        img: "https://www.richersounds.com/media/catalog/product/4/4/44048e6652e9b8c3a97d91f05e54daa07d908e4e_403780_v3.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Pro-Ject T1 (Black)",
+        info: "Turntable",
+        stars: 5,
+        price: 75,
+        img: "https://www.richersounds.com/media/catalog/product/8/4/84ccfd041f5bb039e9f19aa301364126ee46e088_406295.jpeg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Pro-Ject Debut III S Audiophile (Black) ",
+        info: "Turntable",
+        stars: 4.6,
+        price: 109,
+        img: "https://www.richersounds.com/media/catalog/product/d/8/d83f13920372333532a38b5ef19f856a544663aa_402258.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Pro-Ject Essential III (Black)",
+        info: "Turntable",
+        stars: 4.3,
+        price: 79,
+        img: "https://www.richersounds.com/media/catalog/product/f/9/f905d999af60c9d9b8317d04d61131008f302c8f_317776.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Pro-Ject T1 Phono SB (Black)",
+        info: "Turntable",
+        stars: 5,
+        price: 89,
+        img: "https://www.richersounds.com/media/catalog/product/5/b/5b000b608385169c2be04d981236bfff338ec2fd_406312.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Pro-Ject T1 Phono SB (Walnut)",
+        info: "Turntable",
+        stars: 5,
+        price: 139,
+        img: "https://www.richersounds.com/media/catalog/product/c/a/ca23545f3e8bf562701202883eaa7fa907ca64a2_406313.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Pro-Ject Debut Carbon DC (Red) ",
+        info: "Turntable",
+        stars: 4.2,
+        price: 139,
+        img: "https://www.richersounds.com/media/catalog/product/a/7/a7d214c10c85212c91e0e98825bb4c111aecef2e_310464_v2.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Pro-Ject Essential III A (Black)",
+        info: "Turntable",
+        stars: 5,
+        price: 159,
+        img: "https://www.richersounds.com/media/catalog/product/0/9/09707293f0a68445cbd9ae95b52477a3d5568182_318626.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    },
+    {
+        title: "Pro-Ject Essential III Digtal (Black) ",
+        info: "Turntable",
+        stars: 5,
+        price: 139,
+        img: "https://www.richersounds.com/media/catalog/product/d/d/dd3fee8b3f4384aaba6cdc203459983afdd40224_400699.jpg?width=240&height=300&canvas=240:300&quality=80&bg-color=255,255,255&fit=bounds&dpr=2"
+    }
+]
