d26eb5b86353d95c4dbd5d237ee1950a6b73ffb0
Mark Waters <mark.waters@johnlewis.co.uk>
2020-05-13 15:45:36 +0100

    tidy stars component

--- src/components/stars/stars.js
+++ src/components/stars/stars.js
@@ -2,22 +2,18 @@ import React from 'react'
 import './stars.css'
 
 function Stars(props) {
-
-    let rating = props.stars
-    let wholeNumber = Math.floor(rating)
-    let remainder = rating % 1
-    let loopAmount = (remainder > 0) ? (wholeNumber + 1) : wholeNumber
+    const wholeStars = Math.floor(props.stars)
+    const remainderStars = props.stars % 1
+    const loopAmount = (remainderStars > 0) ? (wholeStars + 1) : wholeStars
+    const lastLoopIndex = loopAmount - 1
     const items = []
 
-    console.log(remainder)
-
     for (let index = 0; index < loopAmount; index++) {
-
-        if (index === (loopAmount - 1) && (remainder > 0) && (remainder < 0.5)) {
-            // don't add star
-        } else if (index === (loopAmount - 1) && (remainder > 0) && (remainder < 0.9)) {
+        if (index === lastLoopIndex && (remainderStars === 0)) {
+            items.push(<img key={index} className='star' src={require('./star.png')}></img>)
+        } else if (index === lastLoopIndex && (remainderStars >= 0.5) && (remainderStars < 0.99)) {
             items.push(<img key={index} className='star' src={require('./star-half.png')}></img>)
-        } else {
+        } else if (index != lastLoopIndex) {
             items.push(<img key={index} className='star' src={require('./star.png')}></img>)
         }
     }
