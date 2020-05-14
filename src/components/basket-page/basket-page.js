228938e789412f3e8f463e4cf3d75a6fd4c490a3
Mark Waters <mark.waters@johnlewis.co.uk>
2020-05-14 09:42:13 +0100

    create rough basket page layout

--- src/components/basket-page/basket-page.js
+++ src/components/basket-page/basket-page.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './basket-page.css'
 
 function BasketPage() {
     return (
-        <div>
-            <h1>Basket Page</h1>
+
+        <div className='basket-header-container'>
+            <h1 className='basket-header'>Basket</h1>
+            <div className='basket-flex'>
+                <div className='basket-product-container'>
+                    <div className='one'>Prod 1</div>
+                    <div className='three'>Prod 2</div>
+                    <div className='four'>Prod 4</div>
+                </div>
+                <div className = 'two'>Summary</div>
+            </div>
         </div>
+        
     )
 }
 
